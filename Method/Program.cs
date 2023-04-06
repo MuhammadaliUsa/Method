a717283b3e5eb418c0e77c912e8bf8f85b5b7d84
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-04-06 11:30:28 +0500

    Task1

---
+++ Method/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Method
+{
+    internal class Program
+    {     
+
+        static bool CheckNumber(int number )
+        {
+            if (number>0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the your number: ");
+            int.TryParse(Console.ReadLine(), out int number);
+            bool check = CheckNumber(number);
+            if (check==true)
+            {
+                Console.WriteLine("Your number is positive!");
+            }
+            else
+            {
+                Console.WriteLine("Your number is negative!");
+            }
+        }
+    }
+}
